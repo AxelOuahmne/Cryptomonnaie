9ccf5637923f6c7c435245ed3d37efe7c64766da
Your Name <dantouati18@gmail.com>
2023-11-14 15:54:23 +0100

    progression nav bar

--- frontend/src/templates/NavBar.js
+++ frontend/src/templates/NavBar.js
@@ -1,48 +1,28 @@
-import React from 'react'
-import styles from '../asset/css/mystyle.module.css'; 
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-const NavBar =() =>{
+const NavigationBar = () => {
   return (
-    // navbar navbar-dark bg-dark fixed-top
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark pe-2">
-  <a className="navbar-brand " href="#"><h1> Cryptomonnaie</h1></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse " id="navbarNav">
-    <ul className="navbar-nav ms-5" >
-      <li className="nav-item">
-        <a className={`nav-link ${styles.link}`} href="#" >Features</a>
-      </li>
-      <li className="nav-item">
-        <a className={`nav-link ${styles.link}`} href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className={`nav-link ${styles.link}`} href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className={`nav-link ${styles.link}`} href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className={`nav-link ${styles.link}`} href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className={`nav-link ${styles.link}`}href="#">Pricing</a>
-      </li>
-    </ul>
-    <ul className="navbar-nav  ms-auto ps-2">
-      <li className="nav-item">
-        <a className=" btn btn-danger me-2" href="#">Connexion</a>
-      </li>
-      <li className="nav-item">
-        <a className="btn btn-primary" href="#">S'inscrire</a>
-      </li>
-   
-    </ul>
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home">CoinMarketCap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Accueil</Nav.Link>
+              <Nav.Link href="#cryptocurrencies">Cryptomonnaies</Nav.Link>
+              <Nav.Link href="#exchanges">Échanges</Nav.Link>
+              <Nav.Link href="#news">Actualités</Nav.Link>
+              <Nav.Link href="#portfolio">Portefeuille</Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link href="#signin">Se connecter</Nav.Link>
+              <Nav.Link href="#signup">S'inscrire</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  );
+};
 
-  </div>
-</nav>
-  )
-}
-
-export default NavBar
+export default NavigationBar;
