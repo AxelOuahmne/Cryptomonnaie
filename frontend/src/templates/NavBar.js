765ef9c057f33240ba5a3d8cc4c254ce41da946a
Your Name <dantouati18@gmail.com>
2023-11-14 17:15:20 +0100

    backend avec login,jwt,signup

--- frontend/src/templates/NavBar.js
+++ frontend/src/templates/NavBar.js
@@ -1,27 +1,33 @@
+// NavigationBar.js
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import CryptoList from './CryptoList'; // Assurez-vous que le chemin du composant est correct
 
 const NavigationBar = () => {
   return (
-      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-        <Container>
-          <Navbar.Brand href="#home">CoinMarketCap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Accueil</Nav.Link>
-              <Nav.Link href="#cryptocurrencies">Cryptomonnaies</Nav.Link>
-              <Nav.Link href="#exchanges">Échanges</Nav.Link>
-              <Nav.Link href="#news">Actualités</Nav.Link>
-              <Nav.Link href="#portfolio">Portefeuille</Nav.Link>
-            </Nav>
-            <Nav className="ml-auto">
-              <Nav.Link href="#signin">Se connecter</Nav.Link>
-              <Nav.Link href="#signup">S'inscrire</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div>
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+          <Container>
+            <Navbar.Brand href="#home">CoinMarketCap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Accueil</Nav.Link>
+                <Nav.Link href="#cryptocurrencies">Cryptomonnaies</Nav.Link>
+                <Nav.Link href="#exchanges">Échanges</Nav.Link>
+                <Nav.Link href="#news">Actualités</Nav.Link>
+                <Nav.Link href="#portfolio">Portefeuille</Nav.Link>
+              </Nav>
+              <Nav className="ml-auto">
+                <Nav.Link href="#signin">Se connecter</Nav.Link>
+                <Nav.Link href="#signup">S'inscrire</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <CryptoList />
+      </div>
   );
 };
 
