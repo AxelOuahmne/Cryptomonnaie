467a733dbb87141631d6766b885e268c6775b9da
nathanibgui <ibguinathan@gmail.com>
2023-11-20 15:02:40 +0100

    modif mehdi

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
@@ -39,4 +40,78 @@ exports.login = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
+};
+
+//medhi
+exports.deleteUser = (req, res, next) => {
+    User.deleteOne({ _id: req.params.id })
+        .then(() => {
+            res.status(200).json({
+                message: "Deleted!",
+            });
+        })
+        .catch((error) => {
+            res.status(400).json({
+                error: error,
+            });
+        });
+};
+
+exports.putUser = (req, res, next) => {
+    const userId = req.params.id; // Assuming your route has a parameter named 'id'
+
+    // Assuming you want to update the email based on the request body
+    //const updateNickname = req.body.nickname;
+
+    //Va falloir hasher le mdp
+    const updatePasswd = req.body.password;
+
+    // Use findByIdAndUpdate to find the user by ID and update the email
+    User.findByIdAndUpdate(
+        userId,
+        { password: updatePasswd },
+        { new: true, runValidators: true } // Options to return the updated document and run validators
+    )
+        .then((updatedUser) => {
+            if (!updatedUser) {
+                // If no user found, send a 404 response
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            // Send a success response
+            res
+                .status(200)
+                .json({ message: "User updated successfully", user: updatedUser });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ message: "Internal Server Error" });
+        });
+};
+
+exports.getUser = (req, res, next) => {
+    User.findOne({
+        _id: req.params.id,
+    })
+        .then((thing) => {
+            res.status(200).json(thing);
+        })
+        .catch((error) => {
+            res.status(404).json({
+                error: error,
+            });
+        });
+};
+
+exports.logout = (req, res) => {
+    // Implementation for logout
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Error destroying session:", err);
+            return res.status(500).json({ message: "Internal Server Error" });
+        }
+
+        // Send a response indicating successful logout
+        res.status(200).json({ message: "Logout successful" });
+    });
 };
